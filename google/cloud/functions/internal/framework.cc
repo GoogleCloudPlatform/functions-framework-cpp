525800536e38b8bfdb2ebc47e84b64638301f83f
Carlos O'Ryan <coryan@google.com>
2020-11-11 17:48:21 +0000

    feat: initial version of the framework
    Add `google::cloud::functions::Run()`, a function to implement simple
    HTTP servers.

---
+++ google/cloud/functions/internal/framework.cc
@@ -0,0 +1,107 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/functions/internal/framework.h"
+#include "google/cloud/functions/internal/call_user_function.h"
+#include "google/cloud/functions/internal/parse_options.h"
+#include "google/cloud/functions/version.h"
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/program_options.hpp>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <thread>
+
+namespace google::cloud::functions_internal {
+inline namespace FUNCTIONS_FRAMEWORK_CPP_NS {
+namespace {
+namespace be = boost::beast;
+namespace asio = boost::asio;
+using tcp = boost::asio::ip::tcp;
+
+void HandleSession(tcp::socket socket, HttpFunction const& user_function) {
+  auto report_error = [](be::error_code ec, char const* what) {
+    // TODO(#35) - maybe replace with Boost.Log
+    std::cerr << what << ": " << ec.message() << "\n";
+  };
+  be::error_code ec;
+  for (;;) {
+    be::flat_buffer buffer;
+    // Read a request
+    BeastRequest request;
+    be::http::read(socket, buffer, request, ec);
+    if (ec == be::http::error::end_of_stream) break;
+    if (ec) return report_error(ec, "read");
+    auto const keep_alive = request.keep_alive();
+    auto response = CallUserFunction(user_function, std::move(request));
+    // TODO(#23) - maybe this is enough to declare success.
+    std::cout << std::flush;
+    std::cerr << std::flush;
+    // Send the response
+    response.set(be::http::field::server, BOOST_BEAST_VERSION_STRING);
+    response.prepare_payload();
+    response.keep_alive(keep_alive);
+    be::http::write(socket, response, ec);
+    if (ec) return report_error(ec, "write");
+  }
+  socket.shutdown(tcp::socket::shutdown_send, ec);
+};
+
+}  // namespace
+
+int RunForTest(int argc, char const* const argv[], HttpFunction handler,
+               std::function<bool()> const& shutdown,
+               std::function<void(int)> const& actual_port) {
+  auto vm = ParseOptions(argc, argv);
+  if (vm.count("help") != 0) return 0;
+
+  auto address = asio::ip::make_address(vm["address"].as<std::string>());
+  auto port = vm["port"].as<int>();
+
+  // TODO(#35) - maybe replace with Boost.Log
+  asio::io_context ioc{/*concurrency_hint=*/1};
+  tcp::acceptor acceptor{ioc, {address, static_cast<std::uint16_t>(port)}};
+  actual_port(acceptor.local_endpoint().port());
+
+  auto handle_session = [h = std::move(handler)](tcp::socket socket) {
+    HandleSession(std::move(socket), h);
+  };
+
+  while (!shutdown()) {
+    auto socket = acceptor.accept(ioc);
+    if (!socket.is_open()) break;
+    // Run a thread per-session, transferring ownership of the socket
+    (void)std::async(std::launch::async, handle_session, std::move(socket));
+  }
+  return 0;
+}
+
+int Run(int argc, char const* const argv[], HttpFunction handler) noexcept try {
+  return RunForTest(
+      argc, argv, std::move(handler), [] { return false; },
+      [](int /*unused*/) {});
+} catch (std::exception const& ex) {
+  std::cerr << "Standard C++ exception thrown " << ex.what() << "\n";
+  return 1;
+} catch (...) {
+  std::cerr << "Unknown exception thrown\n";
+  return 1;
+}
+
+}  // namespace FUNCTIONS_FRAMEWORK_CPP_NS
+}  // namespace google::cloud::functions_internal
---
+++ google/cloud/functions/internal/framework_test.cc
@@ -0,0 +1,84 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/functions/internal/framework.h"
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast.hpp>
+#include <gmock/gmock.h>
+#include <cstdlib>
+#include <future>
+#include <iostream>
+#include <string>
+
+namespace google::cloud::functions_internal {
+inline namespace FUNCTIONS_FRAMEWORK_CPP_NS {
+namespace {
+
+char const* const kTestArgv[] = {"unused", "--port=0"};
+auto constexpr kTestArgc = sizeof(kTestArgv) / sizeof(kTestArgv[0]);
+
+std::string HttpGet(std::string const& host, std::string const& port,
+                    std::string const& target) {
+  namespace beast = boost::beast;
+  namespace http = beast::http;
+  using tcp = boost::asio::ip::tcp;
+
+  // Create a socket to make the HTTP request over
+  boost::asio::io_context ioc;
+  tcp::resolver resolver(ioc);
+  beast::tcp_stream stream(ioc);
+  auto const results = resolver.resolve(host, port);
+  stream.connect(results);
+
+  // Use Boost.Beast to make the HTTP request
+  auto constexpr kHttpVersion = 10;  // 1.0 as Boost.Beast spells it
+  http::request<http::string_body> req{http::verb::get, target, kHttpVersion};
+  req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+  req.set(http::field::host, host);
+  http::write(stream, req);
+  beast::flat_buffer buffer;
+  http::response<http::string_body> res;
+  http::read(stream, buffer, res);
+  stream.socket().shutdown(tcp::socket::shutdown_both);
+
+  // Return the body.
+  return std::move(res.body());
+}
+
+TEST(FrameworkTest, Basic) {
+  std::promise<int> port_p;
+  auto port_f = port_p.get_future();
+  std::atomic<bool> shutdown{false};
+  auto hello = [](functions::HttpRequest r) {
+    functions::HttpResponse response;
+    response.set_payload("Hello World from " + r.target());
+    response.set_header("content-type", "text/plain");
+    return response;
+  };
+  auto done = std::async(
+      std::launch::async, RunForTest, kTestArgc, kTestArgv, hello,
+      [&shutdown]() { return shutdown.load(); },
+      [&port_p](int port) mutable { port_p.set_value(port); });
+
+  auto port = port_f.get();
+  auto actual = HttpGet("localhost", std::to_string(port), "/say/hello");
+  EXPECT_EQ(actual, "Hello World from /say/hello");
+  shutdown.store(true);
+  EXPECT_EQ(done.get(), 0);
+}
+
+}  // namespace
+}  // namespace FUNCTIONS_FRAMEWORK_CPP_NS
+}  // namespace google::cloud::functions_internal
