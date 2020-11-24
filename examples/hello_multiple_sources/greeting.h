4c017401de7b4ed75c2f846a259fc518c54a43ef
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-11-24 10:57:33 -0500

    feat: an example with more than one source file (#109)

---
+++ examples/hello_multiple_sources/greeting.h
@@ -0,0 +1,22 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FUNCTIONS_FRAMEWORK_CPP_EXAMPLES_HELLO_MULTIPLE_SOURCES_GREETING_H
+#define FUNCTIONS_FRAMEWORK_CPP_EXAMPLES_HELLO_MULTIPLE_SOURCES_GREETING_H
+
+#include <string>
+
+extern std::string Greeting();
+
+#endif  // FUNCTIONS_FRAMEWORK_CPP_EXAMPLES_HELLO_MULTIPLE_SOURCES_GREETING_H
