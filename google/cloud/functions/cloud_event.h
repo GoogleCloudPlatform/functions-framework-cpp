2072f9c39833ef14a2f62352ab37ad9f209c9673
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-11-25 18:08:52 -0500

    feat: implement wrapper for CloudEvent (#116)

---
+++ google/cloud/functions/cloud_event.h
@@ -0,0 +1,100 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FUNCTIONS_FRAMEWORK_CPP_GOOGLE_CLOUD_FUNCTIONS_CLOUD_EVENT_H
+#define FUNCTIONS_FRAMEWORK_CPP_GOOGLE_CLOUD_FUNCTIONS_CLOUD_EVENT_H
+
+#include "google/cloud/functions/version.h"
+#include <chrono>
+#include <optional>
+#include <string>
+
+namespace google::cloud::functions {
+inline namespace FUNCTIONS_FRAMEWORK_CPP_NS {
+
+/**
+ * Represents a Cloud Event.
+ *
+ * Functions to handle Cloud Events receive an object of this class.
+ *
+ * @see The [Cloud Events Primer][cloud-events-primer] for a general
+ *     introduction to Cloud Events.
+ *
+ * [cloud-events-primer]: https://github.com/cloudevents/spec
+ */
+class CloudEvent {
+ public:
+  inline static auto constexpr kDefaultSpecVersion = "1.0";
+  using ClockType = std::chrono::system_clock;
+  using time_point = ClockType::time_point;
+
+  CloudEvent(std::string id, std::string source, std::string type,
+             std::string spec_version = kDefaultSpecVersion)
+      : id_(std::move(id)),
+        source_(std::move(source)),
+        type_(std::move(type)),
+        spec_version_(std::move(spec_version)) {}
+
+  [[nodiscard]] std::string id() const { return id_; }
+  [[nodiscard]] std::string source() const { return source_; }
+  [[nodiscard]] std::string type() const { return type_; }
+  [[nodiscard]] std::string spec_version() const { return spec_version_; }
+
+  [[nodiscard]] std::optional<std::string> data_content_type() const {
+    return data_content_type_;
+  }
+  [[nodiscard]] std::optional<std::string> data_schema() const {
+    return data_schema_;
+  }
+  [[nodiscard]] std::optional<std::string> subject() const { return subject_; }
+  [[nodiscard]] std::optional<time_point> time() const { return time_; }
+  [[nodiscard]] std::optional<std::string> data() const& { return data_; }
+  [[nodiscard]] std::optional<std::string> data() && {
+    return std::move(data_);
+  }
+
+  void set_data_content_type(std::string v) {
+    data_content_type_ = std::move(v);
+  }
+  void reset_data_content_type() { data_content_type_.reset(); }
+
+  void set_data_schema(std::string v) { data_schema_ = std::move(v); }
+  void reset_data_schema() { data_schema_.reset(); }
+
+  void set_subject(std::string v) { subject_ = std::move(v); }
+  void reset_subject() { subject_.reset(); }
+
+  void set_time(time_point tp) { time_ = tp; }
+  void reset_time() { time_.reset(); }
+
+  void set_data(std::string v) { data_ = std::move(v); }
+  void reset_data() { data_.reset(); }
+
+ private:
+  std::string const id_;
+  std::string const source_;
+  std::string const type_;
+  std::string const spec_version_;
+
+  std::optional<std::string> data_content_type_;
+  std::optional<std::string> data_schema_;
+  std::optional<std::string> subject_;
+  std::optional<time_point> time_;
+  std::optional<std::string> data_;
+};
+
+}  // namespace FUNCTIONS_FRAMEWORK_CPP_NS
+}  // namespace google::cloud::functions
+
+#endif  // FUNCTIONS_FRAMEWORK_CPP_GOOGLE_CLOUD_FUNCTIONS_CLOUD_EVENT_H
---
+++ google/cloud/functions/cloud_event_test.cc
@@ -0,0 +1,98 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "google/cloud/functions/cloud_event.h"
+#include <gmock/gmock.h>
+
+namespace google::cloud::functions {
+inline namespace FUNCTIONS_FRAMEWORK_CPP_NS {
+namespace {
+
+TEST(CloudEventTest, Basic) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  EXPECT_EQ(actual.id(), "test-id");
+  EXPECT_EQ(actual.source(), "test-source");
+  EXPECT_EQ(actual.type(), "test-type");
+  EXPECT_EQ(actual.spec_version(), CloudEvent::kDefaultSpecVersion);
+
+  EXPECT_FALSE(actual.data_content_type().has_value());
+  EXPECT_FALSE(actual.data_schema().has_value());
+  EXPECT_FALSE(actual.subject().has_value());
+  EXPECT_FALSE(actual.time().has_value());
+  EXPECT_FALSE(actual.data().has_value());
+  EXPECT_FALSE(std::move(actual).data().has_value());
+}
+
+TEST(CloudEventTest, WithVersion) {
+  auto const actual = CloudEvent("test-id", "test-source", "test-type", "1.1");
+  EXPECT_EQ(actual.spec_version(), "1.1");
+}
+
+TEST(CloudEventTest, DataContentType) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  actual.set_data_content_type("test-value");
+  ASSERT_TRUE(actual.data_content_type().has_value());
+  EXPECT_EQ(*actual.data_content_type(), "test-value");
+  actual.reset_data_content_type();
+  EXPECT_FALSE(actual.data_content_type().has_value());
+}
+
+TEST(CloudEventTest, DataSchema) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  actual.set_data_schema("test-value");
+  ASSERT_TRUE(actual.data_schema().has_value());
+  EXPECT_EQ(*actual.data_schema(), "test-value");
+  actual.reset_data_schema();
+  EXPECT_FALSE(actual.data_schema().has_value());
+}
+
+TEST(CloudEventTest, Subject) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  actual.set_subject("test-value");
+  ASSERT_TRUE(actual.subject().has_value());
+  EXPECT_EQ(*actual.subject(), "test-value");
+  actual.reset_subject();
+  EXPECT_FALSE(actual.subject().has_value());
+}
+
+TEST(CloudEventTest, Time) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  auto const tp = CloudEvent::ClockType::now();
+  actual.set_time(tp);
+  ASSERT_TRUE(actual.time().has_value());
+  EXPECT_EQ(*actual.time(), tp);
+  actual.reset_time();
+  EXPECT_FALSE(actual.time().has_value());
+}
+
+TEST(CloudEventTest, Data) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  actual.set_data("test-value");
+  ASSERT_TRUE(actual.data().has_value());
+  EXPECT_EQ(*actual.data(), "test-value");
+  actual.reset_data();
+  EXPECT_FALSE(actual.data().has_value());
+}
+
+TEST(CloudEventTest, DataMove) {
+  auto actual = CloudEvent("test-id", "test-source", "test-type");
+  actual.set_data("test-value");
+  auto d = std::move(actual).data();
+  ASSERT_TRUE(d.has_value());
+  EXPECT_EQ(*d, "test-value");
+}
+
+}  // namespace
+}  // namespace FUNCTIONS_FRAMEWORK_CPP_NS
+}  // namespace google::cloud::functions
