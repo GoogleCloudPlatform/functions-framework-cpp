b2c35da191a419a2aaca32f2fb77201562478c40
Carlos O'Ryan <coryan@google.com>
2020-12-07 18:48:18 +0000

    Address review comments

--- examples/site/hello_world_content/hello_world_content.cc
+++ examples/site/hello_world_content/hello_world_content.cc
@@ -16,13 +16,20 @@
 #include <google/cloud/functions/http_request.h>
 #include <google/cloud/functions/http_response.h>
 #include <nlohmann/json.hpp>
+#include <algorithm>
+#include <charconv>
+#include <iterator>
 #include <map>
+#include <sstream>
 #include <string>
+#include <vector>
 
 namespace gcf = ::google::cloud::functions;
 
+namespace {
 std::map<std::string, std::string> parse_www_form_urlencoded(
     std::string const& text);
+} // namespace
 
 gcf::HttpResponse hello_world_content(gcf::HttpRequest request) {  // NOLINT
   std::string name;
@@ -46,3 +53,50 @@ gcf::HttpResponse hello_world_content(gcf::HttpRequest request) {  // NOLINT
   return response;
 }
 // [END functions_hello_world_content]
+
+namespace {
+std::string urldecode(std::string const& encoded) {
+  std::string result;
+  for (std::size_t i = 0; i != encoded.size(); ++i) {
+    if (encoded[i] != '%') {
+      result.push_back(encoded[i]);
+      continue;
+    }
+    if (i + 3 <= encoded.size()) {
+      auto constexpr kUrlEncodingBase = 16;
+      char value;
+      auto const* end = encoded.data() + i + 3;
+      auto r =
+          std::from_chars(encoded.data() + i + 1, end, value, kUrlEncodingBase);
+      if (r.ptr == end) {
+        result.push_back(value);
+        i += 2;
+      } else {
+        result.push_back(encoded[i]);
+      }
+    }
+  }
+  return result;
+}
+
+/// Implements a parser for www-form-urlencoded data, a bit tedious, maybe not
+/// the most efficient, but has very minimal dependencies and it is easy to
+/// read.
+std::map<std::string, std::string> parse_www_form_urlencoded(
+    std::string const& text) {
+  auto tokens = [&text] {
+    std::vector<std::string> tokens;
+    std::istringstream is(text);
+    for (std::string tk; std::getline(is, tk, '&'); tokens.push_back(tk)) {
+    }
+    return tokens;
+  }();
+  std::map<std::string, std::string> result;
+  for (auto& tk : tokens) {
+    auto p = tk.find_first_of('=');
+    result.emplace(urldecode(tk.substr(0, p)), urldecode(tk.substr(p + 1)));
+  }
+  return result;
+}
+
+}  // namespace
