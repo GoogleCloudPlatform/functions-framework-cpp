7678ada4fec16adb89ab026a9dcff9a48c4a364d
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-12-08 18:20:28 -0500

    cleanup: use HTTP status codes in examples (#143)

--- examples/site/http_cors/http_cors.cc
+++ examples/site/http_cors/http_cors.cc
@@ -24,8 +24,7 @@ gcf::HttpResponse http_cors(gcf::HttpRequest request) {  // NOLINT
     // Allows GET requests from any origin with the Content-Type header and
     // caches preflight response for an 3600s
     gcf::HttpResponse response;
-    auto constexpr kNoContent = 204;
-    response.set_result(kNoContent);
+    response.set_result(gcf::HttpResponse::kNoContent);
     response.set_header("Access-Control-Allow-Origin", "*");
     response.set_header("Access-Control-Allow-Methods", "GET");
     response.set_header("Access-Control-Allow-Headers", "Content-Type");
