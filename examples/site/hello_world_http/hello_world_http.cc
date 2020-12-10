366bf8b01bfa10f9c9af0cd1f88bebf41b8be581
Carlos O'Ryan <coryan@users.noreply.github.com>
2020-12-10 17:33:55 -0500

    feat: implement functions_helloworld_http example (#153)
    Implement the `helloworld_http` example and fix `helloworld_get`. Seems
    like I got confused when I was reading the examples for other languages.

--- examples/site/hello_world_http/hello_world_http.cc
+++ examples/site/hello_world_http/hello_world_http.cc
@@ -12,17 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// [START functions_helloworld_get]
+// [START functions_helloworld_http]
 #include <google/cloud/functions/http_request.h>
 #include <google/cloud/functions/http_response.h>
 #include <nlohmann/json.hpp>
 
 namespace gcf = ::google::cloud::functions;
 
-gcf::HttpResponse hello_world_get(gcf::HttpRequest) {  // NOLINT
+gcf::HttpResponse hello_world_http(gcf::HttpRequest request) {
+  auto greeting = [r = std::move(request)] {
+    auto request_json = nlohmann::json::parse(r.payload(), /*cb=*/nullptr,
+                                              /*allow_exceptions=*/false);
+    if (request_json.count("name") && request_json["name"].is_string()) {
+      return "Hello " + request_json.value("name", "World") + "!";
+    }
+    return std::string("Hello World!");
+  };
+
   gcf::HttpResponse response;
-  response.set_payload("Hello World!");
+  response.set_payload(greeting());
   response.set_header("content-type", "text/plain");
   return response;
 }
-// [END functions_helloworld_get]
+// [END functions_helloworld_http]
