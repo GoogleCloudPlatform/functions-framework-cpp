ce7fae0f474c9bb234946f4a5045bf077a0f2ffb
Carlos O'Ryan <coryan@google.com>
2021-02-11 16:10:17 +0000

    tidy things up

--- examples/site/howto_local_development/local_server.cc
+++ examples/site/howto_local_development/local_server.cc
@@ -19,7 +19,7 @@ namespace gcf = ::google::cloud::functions;
 
 namespace {
 
-gcf::HttpResponse HelloWithShutdown(gcf::HttpRequest const& request) {
+gcf::HttpResponse HelloWithShutdown(gcf::HttpRequest const& /*request*/) {
   gcf::HttpResponse response;
   response.set_header("Content-Type", "text/plain");
   response.set_payload("Hello World\n");
