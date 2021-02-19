c071fbc5f58c5f17e4555ea5d84e447a5917160c
Carlos O'Ryan <coryan@users.noreply.github.com>
2021-02-19 14:50:24 -0500

    feat: implement bearer_token example (#283)

--- examples/site_test.cc
+++ examples/site_test.cc
@@ -20,8 +20,13 @@
 #include <cppcodec/base64_rfc4648.hpp>
 #include <gmock/gmock.h>
 #include <nlohmann/json.hpp>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <random>
 
 namespace gcf = ::google::cloud::functions;
+extern gcf::HttpResponse bearer_token(gcf::HttpRequest request);
 extern gcf::HttpResponse http_content(gcf::HttpRequest request);
 extern gcf::HttpResponse hello_world_get(gcf::HttpRequest request);
 extern gcf::HttpResponse hello_world_http(gcf::HttpRequest request);
@@ -51,6 +56,85 @@ namespace {
 using ::testing::AllOf;
 using ::testing::HasSubstr;
 using ::testing::IsEmpty;
+
+TEST(ExamplesSiteTest, BearerToken) {
+  google::cloud::functions_internal::SetEnv("TARGET_URL", std::nullopt);
+  google::cloud::functions_internal::SetEnv("GOOGLE_APPLICATION_CREDENTIALS",
+                                            "/dev/null");
+
+  EXPECT_THROW(bearer_token(gcf::HttpRequest{}), std::exception);
+
+  google::cloud::functions_internal::SetEnv(
+      "TARGET_URL",
+      "https://storage.googleapis.com/storage/v1/"
+      "b?project=invalid-project-name---");
+  EXPECT_THROW(bearer_token(gcf::HttpRequest{}), std::exception);
+
+  // This is a syntactically valid JSON key file, but the key has been
+  // invalidated and therefore presents no security risks.
+  auto const keyfile = nlohmann::json{
+      {"type", "service_account"},
+      {"project_id", "invalid-project-name---"},
+      {"private_key_id", "a1a111aa1111a11a11a11aa111a111a1a1111111"},
+      {"private_key",
+       R"""(-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCltiF2oP3KJJ+S
+tTc1McylY+TuAi3AdohX7mmqIjd8a3eBYDHs7FlnUrFC4CRijCr0rUqYfg2pmk4a
+6TaKbQRAhWDJ7XD931g7EBvCtd8+JQBNWVKnP9ByJUaO0hWVniM50KTsWtyX3up/
+fS0W2R8Cyx4yvasE8QHH8gnNGtr94iiORDC7De2BwHi/iU8FxMVJAIyDLNfyk0hN
+eheYKfIDBgJV2v6VaCOGWaZyEuD0FJ6wFeLybFBwibrLIBE5Y/StCrZoVZ5LocFP
+T4o8kT7bU6yonudSCyNMedYmqHj/iF8B2UN1WrYx8zvoDqZk0nxIglmEYKn/6U7U
+gyETGcW9AgMBAAECggEAC231vmkpwA7JG9UYbviVmSW79UecsLzsOAZnbtbn1VLT
+Pg7sup7tprD/LXHoyIxK7S/jqINvPU65iuUhgCg3Rhz8+UiBhd0pCH/arlIdiPuD
+2xHpX8RIxAq6pGCsoPJ0kwkHSw8UTnxPV8ZCPSRyHV71oQHQgSl/WjNhRi6PQroB
+Sqc/pS1m09cTwyKQIopBBVayRzmI2BtBxyhQp9I8t5b7PYkEZDQlbdq0j5Xipoov
+9EW0+Zvkh1FGNig8IJ9Wp+SZi3rd7KLpkyKPY7BK/g0nXBkDxn019cET0SdJOHQG
+DiHiv4yTRsDCHZhtEbAMKZEpku4WxtQ+JjR31l8ueQKBgQDkO2oC8gi6vQDcx/CX
+Z23x2ZUyar6i0BQ8eJFAEN+IiUapEeCVazuxJSt4RjYfwSa/p117jdZGEWD0GxMC
++iAXlc5LlrrWs4MWUc0AHTgXna28/vii3ltcsI0AjWMqaybhBTTNbMFa2/fV2OX2
+UimuFyBWbzVc3Zb9KAG4Y7OmJQKBgQC5324IjXPq5oH8UWZTdJPuO2cgRsvKmR/r
+9zl4loRjkS7FiOMfzAgUiXfH9XCnvwXMqJpuMw2PEUjUT+OyWjJONEK4qGFJkbN5
+3ykc7p5V7iPPc7Zxj4mFvJ1xjkcj+i5LY8Me+gL5mGIrJ2j8hbuv7f+PWIauyjnp
+Nx/0GVFRuQKBgGNT4D1L7LSokPmFIpYh811wHliE0Fa3TDdNGZnSPhaD9/aYyy78
+LkxYKuT7WY7UVvLN+gdNoVV5NsLGDa4cAV+CWPfYr5PFKGXMT/Wewcy1WOmJ5des
+AgMC6zq0TdYmMBN6WpKUpEnQtbmh3eMnuvADLJWxbH3wCkg+4xDGg2bpAoGAYRNk
+MGtQQzqoYNNSkfus1xuHPMA8508Z8O9pwKU795R3zQs1NAInpjI1sOVrNPD7Ymwc
+W7mmNzZbxycCUL/yzg1VW4P1a6sBBYGbw1SMtWxun4ZbnuvMc2CTCh+43/1l+FHe
+Mmt46kq/2rH2jwx5feTbOE6P6PINVNRJh/9BDWECgYEAsCWcH9D3cI/QDeLG1ao7
+rE2NcknP8N783edM07Z/zxWsIsXhBPY3gjHVz2LDl+QHgPWhGML62M0ja/6SsJW3
+YvLLIc82V7eqcVJTZtaFkuht68qu/Jn1ezbzJMJ4YXDYo1+KFi+2CAGR06QILb+I
+lUtj+/nH3HDQjM4ltYfTPUg=
+-----END PRIVATE KEY-----
+)"""},
+      {"client_email",
+       "invalid-service-account@invalid-project.iam.gserviceaccount.com"},
+      {"client_id", "100000000000000000001"},
+      {"auth_uri", "https://accounts.google.com/o/oauth2/auth"},
+      {"token_uri", "https://oauth2.googleapis.com/token"},
+      {"auth_provider_x509_cert_url",
+       "https://www.googleapis.com/oauth2/v1/certs"},
+      {"client_x509_cert_url",
+       "https://www.googleapis.com/robot/v1/metadata/x509/"
+       "foo-email%40foo-project.iam.gserviceaccount.com"},
+  };
+
+  std::mt19937_64 gen(std::random_device{}());
+  auto rnd = [&gen] {
+    return std::to_string(std::uniform_int_distribution<std::uint64_t>()(gen));
+  };
+  auto const filename =
+      std::filesystem::path(::testing::TempDir()) / (rnd() + '-' + rnd());
+  std::ofstream(filename) << keyfile.dump() << "\n";
+
+  google::cloud::functions_internal::SetEnv("GOOGLE_APPLICATION_CREDENTIALS",
+                                            filename);
+  EXPECT_THROW(bearer_token(gcf::HttpRequest{}), std::exception);
+
+  EXPECT_NO_THROW(
+      bearer_token(gcf::HttpRequest{}.set_target("/no-auth-header")));
+
+  std::filesystem::remove(filename);
+}
 
 TEST(ExamplesSiteTest, HelloWorldContent) {
   auto make_request = [](std::string content_type, std::string payload) {
