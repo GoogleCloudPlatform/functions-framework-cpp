a43b9396aa214b8e5316c756a2e6f992f50d7eb1
Carlos O'Ryan <coryan@google.com>
2021-02-28 10:41:30 -0500

    feat: more efficient access to HttpRequest data
    Some applications may want to move the contents of a HttpRequest.

--- google/cloud/functions/http_request.h
+++ google/cloud/functions/http_request.h
@@ -41,7 +41,8 @@ class HttpRequest {
   [[nodiscard]] std::string const& target() const { return target_; }
 
   /// The request payload
-  [[nodiscard]] std::string const& payload() const { return payload_; }
+  [[nodiscard]] std::string const& payload() const & { return payload_; }
+  [[nodiscard]] std::string&& payload() && { return std::move(payload_); }
 
   /// The request HTTP headers
   [[nodiscard]] HeadersType const& headers() const { return headers_; }
