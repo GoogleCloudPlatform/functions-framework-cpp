2e3d900cae927e2d0c77f3e55a66b55bec7c50a3
Scott Hart <sdhart@google.com>
2024-11-04 15:00:50 -0500

    chore: modernize repo versions and dependencies (#422)

--- build_scripts/vcpkg-overlays/portfile.cmake
+++ build_scripts/vcpkg-overlays/portfile.cmake
@@ -1,10 +1,12 @@
 set(SOURCE_PATH "/usr/local/share/gcf")
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH} PREFER_NINJA
-                      DISABLE_PARALLEL_CONFIGURE OPTIONS -DBUILD_TESTING=OFF)
-
-vcpkg_install_cmake(ADD_BIN_TO_PATH)
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} DISABLE_PARALLEL_CONFIGURE
+                      OPTIONS -DBUILD_TESTING=OFF)
+vcpkg_cmake_install(ADD_BIN_TO_PATH)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+# vcpkg_fixup_cmake_targets is marked as deprecated and vcpkg_cmake_config_fixup
+# is listed as its replacement. However, for our purposes, it is not a drop in
+# replacement and additional work needs to be done if we are to switch.
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
